a0f896bfc9c7569a3d76dbb974d9459e80063cf8
ProgrammerLP <developer.programmerlp@gmx.net>
2026-02-21 16:42:30 +0100

    models

---
+++ src/app/Models/news.ts
@@ -0,0 +1,9 @@
+export type News = {
+    id?: number;
+    title?: string;
+    description?: string;
+    imageUrl?: string;
+    category?: string;
+    content?: string;
+    publishedAt?: string | Date;
+};
