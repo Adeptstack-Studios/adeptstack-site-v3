3a3307659a008bc9f9450fa73fa7226cee744b31
ProgrammerLP <developer.programmerlp@gmx.net>
2026-02-21 16:42:07 +0100

    api calls

---
+++ src/libs/getNews.ts
@@ -0,0 +1,14 @@
+import {News} from "@/app/Models/news";
+import {fetchWithRetry, getBaseUrl} from "@/libs/utils";
+
+export async function getBlogPosts(): Promise<News[]> {
+    const baseUrl = getBaseUrl();
+    return await fetchWithRetry(`${baseUrl}/api/news/get`);
+}
+
+export async function getPostById(id: string): Promise<News | undefined> {
+    const baseUrl = getBaseUrl();
+    const data = await fetchWithRetry(`${baseUrl}/api/news/get/${id}`);
+
+    return Array.isArray(data) && data.length === 0 ? undefined : data;
+}
