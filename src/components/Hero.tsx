3e501408a0b4fe90977ee90c87dafcf538063e0a
ProgrammerLP <info@adeptstack.net>
2026-07-19 18:35:26 +0200

    remove link to newsletter

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -52,11 +52,11 @@ export default function Hero() {
                             </button>
                         </a>
 
-                        <a href={"/newsletter"}>
-                            <button className="px-8 py-4 border-2 border-blue-600 hover:border-blue-500 hover:bg-blue-500 text-white rounded-xl font-bold text-lg transition shadow-lg shadow-blue-900/20 ">
-                                Newsletter
-                            </button>
-                        </a>
+                        {/*<a href={"/newsletter"}>*/}
+                        {/*    <button className="px-8 py-4 border-2 border-blue-600 hover:border-blue-500 hover:bg-blue-500 text-white rounded-xl font-bold text-lg transition shadow-lg shadow-blue-900/20 ">*/}
+                        {/*        Newsletter*/}
+                        {/*    </button>*/}
+                        {/*</a>*/}
                     </div>
                 </div>
                 <div className="hidden lg:block"></div>
